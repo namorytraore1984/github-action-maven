ee958eb346767d62513a9e03288d19db2272d702
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-26 22:53:32 -0500

    1er commit

---
+++ src/main/java/com/example/githubactionmaven/GithubActionMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactionmaven/GithubActionMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
